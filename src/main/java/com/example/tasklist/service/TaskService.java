912a7b25f3be9987bd461cd0f3af3dffe24c5b96
Nursultan <erkebaevnursultan@gmail.com>
2023-10-29 09:36:11 +0600

    add validation, service and controllers and configure all of them.

--- src/main/java/com/example/tasklist/service/TaskService.java
+++ src/main/java/com/example/tasklist/service/TaskService.java
@@ -9,7 +9,7 @@ public interface TaskService {
     Task getById(Long id);
     List<Task> getAllByUserId(Long id);
     Task update(Task task);
-    Task create(Task task);
+    Task create(Task task, Long id);
     void delete(Long id);
 
 
--- src/main/java/com/example/tasklist/web/dto/user/UserDto.java
+++ src/main/java/com/example/tasklist/web/dto/user/UserDto.java
@@ -1,20 +1,33 @@
 package com.example.tasklist.web.dto.user;
 
-import com.example.tasklist.domain.user.Role;
-import com.example.tasklist.domain.user.Task;
+import com.example.tasklist.web.dto.validation.OnCreate;
+import com.example.tasklist.web.dto.validation.OnUpdate;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
-import java.util.List;
-import java.util.Set;
 
 @Data
 public class UserDto {
 
+    @NotNull(message = "id must be not null.", groups = OnUpdate.class)
     private Long id;
+
+    @NotNull(message = "Name must be not null.", groups = {OnCreate.class, OnUpdate.class})
+    @Length(max = 255, message = "Name length must be smaller than 255 symbol", groups = {OnUpdate.class, OnCreate.class})
     private String name;
+
+    @NotNull(message = "Username must be not null.", groups = {OnCreate.class, OnUpdate.class})
+    @Length(max = 255, message = "Username length must be smaller than 255 symbol", groups = {OnUpdate.class, OnCreate.class})
     private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull(message = "Password must be not null.", groups = {OnCreate.class, OnUpdate.class})
     private String password;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull(message = "PasswordConfirmation must be not null.", groups = {OnCreate.class})
     private String passwordConfirmation;
-    private Set<Role> roles;
-    private List<Task> tasks;
+
 }
